f48aefd675f3f0338ebb9b455029af72f8ddfae8
Lucas Arceno <pulgalucas1131@gmail.com>
2022-11-02 18:45:31 -0300

    Adicionado gravidade

--- Jogador.cpp
+++ Jogador.cpp
@@ -5,6 +5,7 @@ int Jogador::vidaJogador = 100;
 Jogador::Jogador(sf::Vector2f posicao, sf::Vector2f tamanho) : Entidade(posicao, tamanho) ,vidaMaxima(100), velocidadeMax(5.f), velocidadeMaxY(15.f), velocidadeMin(1.f)
 {
 	initVariables();
+	initPhysics();
 }
 
 Jogador::~Jogador()
@@ -13,11 +14,18 @@ Jogador::~Jogador()
 
 void Jogador::initVariables()
 {
-	velocidade.x = 0.3f;
-	velocidade.y = 0.3f;
-	vidaJogador = vidaMaxima;
+	this->velocidade.x = 0.3f;
+	this->velocidade.y = 0.3f;
+	this->vidaJogador = vidaMaxima;
 
-	corpo.setFillColor(sf::Color::Blue);
+	this->corpo.setFillColor(sf::Color::Blue);
+}
+
+void Jogador::initPhysics()
+{
+	this->acceleration = 3.0f;
+	this->drag = 0.87f;
+	this->gravity = 4.f;
 }
 
 const sf::FloatRect Jogador::getGlobalBounds() const
@@ -25,13 +33,54 @@ const sf::FloatRect Jogador::getGlobalBounds() const
 	return sprite.getGlobalBounds();
 }
 
+void Jogador::move(const float dir_x, const float dir_y)
+{
+	//Acceleration
+	this->velocidade.x += dir_x * this->acceleration;
+
+	//Limit velocity
+	if (std::abs(this->velocidade.x) > this->velocidadeMax) {
+		this->velocidade.x = this->velocidadeMax * ((this->velocidade.x < 0.f) ? -1.f : 1.f);
+	}
+}
+
 void Jogador::update()
 {
 	updateMovimento();
+	updatePhysics();
+}
+
+void Jogador::updatePhysics()
+{
+	//Gravity
+	this->velocidade.y += 1.0 * this->gravity;
+	if (std::abs(this->velocidade.x) > this->velocidadeMaxY) {
+		this->velocidade.y = this->velocidadeMaxY * ((this->velocidade.y < 0.f) ? -1.f : 1.f);
+	}
+
+	//Deceleration
+	this->velocidade *= this->drag;
+
+	//Limit deceleration
+	if (std::abs(this->velocidade.x) < this->velocidadeMin) {
+		this->velocidade.x = 0.0f;
+	}
+	if (std::abs(this->velocidade.y) < this->velocidadeMin) {
+		this->velocidade.y = 0.0f;
+	}
+
+	this->corpo.move(this->velocidade);
 }
 
 void Jogador::updateMovimento()
 {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		this->move(-1.0f, 0);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		this->move(1.f, 0);
+	}
+	/*
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 		corpo.move(-velocidade.x, 0);
 	}
@@ -44,4 +93,5 @@ void Jogador::updateMovimento()
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
 		corpo.move(0, velocidade.y);
 	}
+	*/
 }
--- Jogador.h
+++ Jogador.h
@@ -5,22 +5,33 @@ class Jogador : public Entidade
 {
 private:
 	sf::Sprite sprite;
+
 	sf::Vector2f velocidade;
 	const int velocidadeMax;
 	const int velocidadeMin;
-	const int velocidadeMaxY;
+
 	const int vidaMaxima;
 	static int vidaJogador;
+
+	//Physics
+	float acceleration;
+	float drag;
+	float gravity;
+	const int velocidadeMaxY;
 
 public:
 	Jogador(sf::Vector2f posicao = sf::Vector2f(0.f, 0.f), sf::Vector2f tamanho = sf::Vector2f(50.f, 50.f));
 	~Jogador();
 
 	void initVariables();
+	void initPhysics();
 
 	const sf::FloatRect getGlobalBounds() const;
 
+	void move(const float dir_x, const float dir_y);
+
 	void update();
+	void updatePhysics();
 	void updateMovimento();
 };
 
