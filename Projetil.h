8928c3547820201a6cbb2cabad736cb0c50dde3d
Rick <rick.sonikee@gmail.com>
2022-11-07 03:12:26 -0300

    Update projetil

--- Projetil.h
+++ Projetil.h
@@ -1,16 +1,21 @@
 #pragma once
 #include "Entidade.h"
+#include "Jogador.h"
 #include <math.h>
 
 class Projetil : public Entidade
 {
 private:
-	bool colisaoProjetil = 0;
-	//sf::Vector2f posJogador;
-	sf::Vector2f Direcao; // Coordenadas que o projetil deverá ir ( diferença de pos do morcego e do player )
+	
+	bool colisaoProjetil = 1;
 	int contTempVida = 0;
-
+	// Coordenadas que o projetil deverá ir ( diferença de pos do morcego e do player )
+	sf::Vector2f Direcao; 
+	sf::Vector2f posJogador;
+	
 public:
+
+	Jogador* pJogador;
 
 	Projetil(sf::Vector2f posicao = sf::Vector2f(5.f, 5.f), sf::Vector2f tamanho = sf::Vector2f(20.f, 10.f));
 	~Projetil();
@@ -21,8 +26,8 @@ public:
 
 	// Gambiarra, alterar os parametros de update e updatemovimento transforma a classe em abstrata -- Procurar solução melhor
 	// Atualmente recebendo separadamente o valor x e y da posição -- Não consegui passar como um vector2f
-	void updateProjetil(float posX, float posY, float JposX, float JposY);
-	void updateMovimentoProjetil(float posX, float posY, float JposX, float JposY);
-
+	void updateProjetil(float posX, float posY);
+	void updateMovimentoProjetil(float posX, float posY);
+	sf::Vector2f normalizedVector(sf::Vector2f direcao); // Creditos : www.youtube.com/watch?v=_Qc_cj1Nggw&list=LL&index=14
 };
 
